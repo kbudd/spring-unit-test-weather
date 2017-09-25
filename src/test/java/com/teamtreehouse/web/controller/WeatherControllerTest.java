b8ed4a7af4908c2f73b1300093b0d78886a93c25
kylebudd <thekbudd@gmail.com>
2017-09-25 08:52:30 -0400

    End of video 3

--- src/test/java/com/teamtreehouse/web/controller/WeatherControllerTest.java
+++ src/test/java/com/teamtreehouse/web/controller/WeatherControllerTest.java
@@ -1,5 +1,33 @@
 package com.teamtreehouse.web.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 public class WeatherControllerTest {
+    private MockMvc mockMvc;
+    private WeatherController weatherController;
+
+    @Before
+    public void setUp() {
+        weatherController = new WeatherController();
+        mockMvc = new MockMvcBuilders().standaloneSetup(weatherController).build();
+    }
+
+    @Test
+    public void home_shouldRenderDetailView() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("weather/detail"));
+    }
+
+    @Test
+    public void search_ShouldRedirectWithPathParameter() throws Exception {
+        mockMvc.perform(get("/search").param("q", "33596"))
+                .andExpect(redirectedUrl("/search/33596"));
+    }
 
 }
