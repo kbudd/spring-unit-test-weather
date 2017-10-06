fccc882d2748b9f0aa2cbaa0ad187cc0d67c0f60
kylebudd <thekbudd@gmail.com>
2017-10-06 15:27:02 -0400

    Done

--- src/test/java/com/teamtreehouse/dao/FavoriteDaoTest.java
+++ src/test/java/com/teamtreehouse/dao/FavoriteDaoTest.java
@@ -3,10 +3,6 @@ package com.teamtreehouse.dao;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.teamtreehouse.Application;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-
 import com.teamtreehouse.domain.Favorite;
 import com.teamtreehouse.domain.User;
 import org.junit.Before;
@@ -20,14 +16,13 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-/**
- * Created by kylebudd on 10/6/17.
- */
-
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static com.teamtreehouse.domain.Favorite.FavoriteBuilder;
 
+/**
+ * Created by kylebudd on 10/6/17.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
 @DatabaseSetup("classpath:favorites.xml")
@@ -38,4 +33,25 @@ import static com.teamtreehouse.domain.Favorite.FavoriteBuilder;
 public class FavoriteDaoTest {
     @Autowired
     private FavoriteDao favoriteDao;
+
+    @Before
+    public void setUp() {
+        User user = new User();
+        user.setId(1L);
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user,null));
+    }
+
+    @Test
+    public void findAll_ShouldReturnTwoFavorites() throws Exception {
+        assertThat(favoriteDao.findAll(),hasSize(2));
+    }
+
+    @Test
+    public void save_ShouldPersistEntity() throws Exception {
+        String placeId = "KBuddTesting1234!";
+        Favorite favorite = new FavoriteBuilder().withPlaceId(placeId).build();
+        favoriteDao.saveForCurrentUser(favorite);
+        assertThat(favoriteDao.findByPlaceId(placeId),notNullValue(Favorite.class));
+    }
+
 }
